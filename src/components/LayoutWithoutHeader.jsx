95b83b04ca1f68b1fd302ece4302344e44b3ca65
BrunoPCH <bruno.perez.ch@gmail.com>
2023-11-22 17:15:00 -0600

    footer abajo

--- src/components/LayoutWithoutHeader.jsx
+++ src/components/LayoutWithoutHeader.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const LayoutWithoutHeader = () => {
   return (
-    <div className="flex-center">
+    <div className="">
       <Outlet />
       <Footer />
     </div>
