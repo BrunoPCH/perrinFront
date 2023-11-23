1917c41f643c7f94f1fb5e69737bcd07acefc543
BrunoPCH <bruno.perez.ch@gmail.com>
2023-11-23 14:04:20 -0600

    hamburguer menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
+import MovilMenu from "./MovilMenu";
 import "../styles/Header.css";
 import logoImage from "../assets/perrinLogoHeader.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleShowMenuFn = () => {
+    setShowMenu(!showMenu);
+  };
   return (
-    <div className="container-header">
-      <div className="container-boxed-header">
-        <img className="logo-header" src={logoImage} alt="Perrin logo" />
-        <button>=</button>
+    <div>
+      <div className="container-header">
+        <div className="container-boxed-header">
+          <Link to="/">
+            <img className="logo-header" src={logoImage} alt="Perrin logo" />
+          </Link>
+          <button className="hamburguer-button" onClick={toggleShowMenuFn}>
+            <span className="material-symbols-outlined">menu</span>
+          </button>
+        </div>
       </div>
+      <div className="">{showMenu ? <MovilMenu /> : <></>}</div>
     </div>
   );
 };
---
+++ src/components/MovilMenu.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import "../styles/MovilMenu.css";
+
+const rutes = [
+  { page: "Home", path: "/", id: 1 },
+  { page: "Login", path: "/login", id: 4 },
+  { page: "Registrar", path: "/register", id: 2 },
+  { page: "Registrar Perrin", path: "/register-dog", id: 3 },
+];
+const MovilMenu = () => {
+  const ruteItems = rutes.map((rute) => (
+    <Link key={rute.id} className="movilmenu-item" to={rute.path}>
+      {rute.page}
+    </Link>
+  ));
+
+  return (
+    <div className="movilmenu-container">
+      <div className="movilmenu-header"></div>
+      <div className="movilemenu-content">{ruteItems}</div>
+    </div>
+  );
+};
+
+export default MovilMenu;
